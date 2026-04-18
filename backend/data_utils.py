8ae99fec134b51adaeb42c699ad640114ec83ee4
Arielle <arielleaventura15@gmail.com>
2026-04-18 12:31:40 +0800

    Implement data access layer

--- backend/data_utils.py
+++ backend/data_utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 
-DATA_PATH = os.path.join("..", "ml-research", "datasets", "sea_fuel_prices_2020_2026 - Sheet1.csv")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "machine-learning", "dataset", "sea_fuel_prices_2020_2026.csv")
+
 
 def get_latest_country_stats(country_name: str):
     """Fetches the most recent economic markers for a specific country."""
