9a2b8bee9054904842692a5f09f71c24de46eaf7
Arielle <arielleaventura15@gmail.com>
2026-04-21 17:36:58 +0800

    feat: implement WIP main interface

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,8 +3,8 @@ import PredictionResults from "@/app/components/prediction-results";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col items-center justify-center py-32 px-16">
+    <div className="flex flex-col flex-1 bg-[#F2F4F9] font-sans">
+      <main className="flex flex-1 w-full flex-col items-center justify-center px-6 py-16 md:px-16 md:py-24">
         <FuelPriceSimulation />
         <PredictionResults />
       </main>
