d9ff6df7db2406bff4c220483df31d2cdb36faf2
dambaculio <dambaculio@addu.edu.ph>
2026-04-21 17:16:09 +0800

    brent-crude-vs-local-trend component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,10 @@
-import PredictionResults from "@/components/prediction-results";
+import PredictionConfidenceDistribution from "@/components/brent-crude-vs-local-trend";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full flex-col items-center justify-center py-32 px-16">
-        <PredictionResults />
+        <PredictionConfidenceDistribution />
       </main>
     </div>
   );
